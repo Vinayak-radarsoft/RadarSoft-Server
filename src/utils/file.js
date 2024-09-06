c8d1792cc9290ba825a87e92e011434885e784ae
vinayak <vinayak.hiwase@radarsoft.tech>
2024-09-06 18:23:07 +0530

    pdf-file-issue-resolved

--- src/utils/file.js
+++ src/utils/file.js
@@ -2,34 +2,30 @@ require("dotenv").config();
 const aws = require("aws-sdk");
 
 const s3 = new aws.S3({
-    accessKeyId: process.env.S3_ACCESS_KEY,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: process.env.S3_BUCKET_REGION,
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: process.env.S3_BUCKET_REGION,
 });
 
 const bucketName = process.env.S3_BUCKET_NAME;
 
-const uploadFile = async (
-    file,
-    optimizeData,
-    existingKey = null
-) => {
-    let folderPath = "Radarsoft/career";
+const uploadFile = async (file, optimizeData, existingKey = null) => {
+  let folderPath = "Radarsoft/career";
 
-    let key = existingKey || `${folderPath}/${Date.now()}_${file.name}`;
-    const contentType = file?.mimetype?.split("/")[1] || 'octet-stream';
+  let key = existingKey || `${folderPath}/${Date.now()}_${file.name}`;
+  const contentType = file?.mimetype || "application/octet-stream";
 
-    console.log("uploadFile here..", file);
-    const params = {
-        Bucket: bucketName,
-        Key: key,
-        Body: optimizeData,
-        ContentType: contentType,
-        ACL: "public-read-write",
-    };
-    const data = await s3.upload(params).promise();
-    console.log("Optimized image uploaded to S3:", data.Location, data.Key);
-    return data.Location; // Return S3 URL
+  console.log("uploadFile here..", contentType, file);
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    Body: optimizeData,
+    ContentType: contentType,
+    ACL: "public-read-write",
+  };
+  const data = await s3.upload(params).promise();
+  console.log("Optimized image uploaded to S3:", data.Location, data.Key);
+  return data.Location; // Return S3 URL
 };
 
-module.exports = uploadFile
+module.exports = uploadFile;
